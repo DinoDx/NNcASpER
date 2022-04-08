a998ebd86c4edec74f67ae0be80ec4ef34aede3b
DinoDx <r.rapacciuolo1@studenti.unisa.it>
2022-04-08 18:56:29 +0200

    Model and first ga implementation added

--- DataPreprocessing.py
+++ DataPreprocessing.py
@@ -3,18 +3,18 @@ import numpy as np
 
 class DataPreprocessing :
 
-    def __init__(self) -> None:
+    def __init__(self):
         pass
 
     global data 
     data = pd.read_csv("complete_Dataset.csv", sep=",")
 
-    def inputData(self, n) -> np.ndarray:
+    def inputData(self, n):
         useless_input_data = ["ComponentName", "nameProject", "M_TextualCohesion","M_TextualEntropy","ComplexClass","LargeClass","LazyClass","RefusedBequest","SpaghettiCode"]
         input_data = np.array(data.drop(useless_input_data, axis=1))[:n]
         return input_data
 
-    def outputData(self, n) -> np.ndarray:
+    def outputData(self, n):
         useless_output_data = ["ComponentName","nameProject","M_CBO","M_CYCLO","M_DIT","M_ELOC","M_FanIn","M_FanIn_1","M_LCOM","M_LOC","M_LOCNAMM","M_NOA","M_NOC","M_NOM","M_NOMNAMM","M_NOPA","M_PMMM","M_PRB","M_WLOCNAMM","M_WMC","M_WMCNAMM","M_TextualCohesion","M_TextualEntropy"]
         output_data = np.array(data.drop(useless_output_data, axis=1))[:n]
         return output_data
--- main.py
+++ main.py
@@ -1,4 +1,78 @@
+import tensorflow
+from tensorflow import keras as k
+import pygad
+import pygad.kerasga as kga
 from DataPreprocessing import DataPreprocessing
 
+# model creation
+input_layer = k.layers.Input(19)
+dense_layer = k.layers.Dense(11, activation="relu")
+output_layer = k.layers.Dense(5, activation="softmax")
+
+model = k.Sequential()
+model.add(input_layer)
+model.add(dense_layer)
+model.add(output_layer)
+
+# ga setup
+num_individuals = 10
+ga = kga.KerasGA(model = model, num_solutions=num_individuals)
+
 d = DataPreprocessing()
-print(d.inputData(2))
+data_inputs = d.inputData(num_individuals)
+data_outputs = d.outputData(num_individuals)
+
+def fitness_func(solution, sol_idx):
+    global data_inputs, data_outputs, ga, model
+
+    model_weights_matrix = kga.model_weights_as_matrix(model=model, weights_vector=solution)
+
+    model.set_weights(weights=model_weights_matrix)
+
+    predictions = model.predict(data_inputs)
+
+    cce = k.losses.CategoricalCrossentropy()
+    solution_fitness = 1.0 / cce(data_outputs, predictions).numpy()
+
+    return solution_fitness
+
+def callback_generation(ga_instance):
+    print("Generation = {generation}".format(generation=ga_instance.generations_completed))
+    print("Fitness    = {fitness}".format(fitness=ga_instance.best_solution()[1]))
+
+
+num_generations = 100
+num_parents_mating = 5
+initial_population = ga.population_weights
+
+ga_instance = pygad.GA(num_generations=num_generations,
+                        num_parents_mating=num_parents_mating,
+                        initial_population=initial_population,
+                        fitness_func=fitness_func,
+                        on_generation=callback_generation)
+
+ga_instance.run()
+
+ga_instance.plot_result(title="PyGAD & Keras - Iteration vs. Fitness", linewidth=4)
+
+# Returning the details of the best solution.
+solution, solution_fitness, solution_idx = ga_instance.best_solution()
+print("Fitness value of the best solution = {solution_fitness}".format(solution_fitness=solution_fitness))
+print("Index of the best solution : {solution_idx}".format(solution_idx=solution_idx))
+
+# Fetch the parameters of the best solution.
+best_solution_weights = pygad.kerasga.model_weights_as_matrix(model=model,
+                                                              weights_vector=solution)
+model.set_weights(best_solution_weights)
+predictions = model.predict(data_inputs)
+# print("Predictions : \n", predictions)
+
+# Calculate the categorical crossentropy for the trained model.
+cce = tensorflow.keras.losses.CategoricalCrossentropy()
+print("Categorical Crossentropy : ", cce(data_outputs, predictions).numpy())
+
+# Calculate the classification accuracy for the trained model.
+ca = tensorflow.keras.metrics.CategoricalAccuracy()
+ca.update_state(data_outputs, predictions)
+accuracy = ca.result().numpy()
+print("Accuracy : ", accuracy)
