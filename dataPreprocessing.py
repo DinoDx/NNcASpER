b3c9e50303c9b50c52f413a9d94653de96a1fab4
DinoDx <r.rapacciuolo1@studenti.unisa.it>
2022-05-13 17:35:16 +0200

    Added data balancing to dataPreprocessing.py

--- dataPreprocessing.py
+++ dataPreprocessing.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
+import imblearn
 from sklearn import preprocessing
+from collections import Counter
+from matplotlib import pyplot
 
 def dataPreprocessing(f, t):
 
@@ -14,13 +17,55 @@ def dataPreprocessing(f, t):
     todrop_output_data = ["ComponentName","nameProject","M_CBO","M_CYCLO","M_DIT","M_ELOC","M_FanIn","M_FanIn_1","M_LCOM","M_LOC",
         "M_LOCNAMM","M_NOA","M_NOC","M_NOM","M_NOMNAMM","M_NOPA","M_PMMM","M_PRB","M_WLOCNAMM","M_WMC","M_WMCNAMM","M_TextualCohesion","M_TextualEntropy"]
 
-    input_data = np.array(data.drop(todrop_input_data, axis=1))[f:t]
-    output_data = np.array(data.drop(todrop_output_data, axis=1))[f:t]
+    x_data = np.array(data.drop(todrop_input_data, axis=1))[f:t]
+    y_data = np.array(data.drop(todrop_output_data, axis=1))[f:t]
                 
     # Freature Scaling
     scaler = preprocessing.MinMaxScaler(feature_range=(0,1))
-    
-    scaled_input_data = scaler.fit_transform(input_data)
-    scaled_output_data = scaler.fit_transform(output_data) 
+    x_data = scaler.fit_transform(x_data)
 
-    return scaled_input_data, scaled_output_data
+    # Data Balancing
+    y_labeled = []
+
+    for target in y_data:
+        if(np.array_equal(target, [0,0,0,0,0])):
+                y_labeled .append(0)
+        elif(np.array_equal(target, [1,0,0,0,0])):
+                y_labeled .append(1)
+        elif(np.array_equal(target, [0,1,0,0,0])):
+                y_labeled .append(2)
+        elif(np.array_equal(target, [0,0,1,0,0])):
+                y_labeled .append(3)
+        elif(np.array_equal(target, [0,0,0,1,0])):
+                y_labeled .append(4)
+        elif(np.array_equal(target, [0,0,0,0,1])):
+                y_labeled .append(5)
+    
+    le = preprocessing.LabelEncoder()
+    y_labeled  = le.fit_transform(y_labeled )
+
+    '''''
+    # Distribution before balancing with SMOTE
+    counter = Counter(labeled_y)
+    for k,v in counter.items():
+        per = v / len(y) * 100
+        print('Class=%d, n=%d (%.3f%%)' % (k, v, per))
+
+    pyplot.bar(counter.keys(), counter.values())
+    pyplot.show()
+    '''''
+
+    x_res, y_res = imblearn.over_sampling.SMOTE().fit_resample(x_data, y_labeled )
+
+    '''''
+    # Distribution after balancing with SMOTE
+    counter = Counter(y_res)
+    for k,v in counter.items():
+        per = v / len(y_data) * 100
+        print('Class=%d, n=%d (%.3f%%)' % (k, v, per))
+
+    pyplot.scatter(counter.keys(), counter.values())
+    pyplot.show()
+    '''''
+
+    return x_res, y_res
